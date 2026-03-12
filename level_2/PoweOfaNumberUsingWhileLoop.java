18836466b243a0685ba4fa6c166c227e9b3258e9
Hyndhavi2007 <hyndhavisannapureddy@gmail.com>
2026-03-12 21:05:34 +0530

    Add Power of a number using while loop java code

---
+++ level_2/PoweOfaNumberUsingWhileLoop.java
@@ -0,0 +1,20 @@
+package level_2;
+import java.util.Scanner;
+public class PoweOfaNumberUsingWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = sc.nextInt();
+        System.out.print("Enter power: ");
+        int power = sc.nextInt();
+        int result = 1;
+        int counter = 0;
+        while (counter < power) {
+            result = result * number;
+            counter++;
+        }
+        System.out.println("Result: " + result);
+        sc.close();
+    }
+}
+
